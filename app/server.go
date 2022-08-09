6b7227020854d2718613afedadcf105dca8cb822
nafanjka <stas.hrytsyk@gmail.com>
2022-08-09 17:22:43 +0300

    revert message changes

--- app/server.go
+++ app/server.go
@@ -10,7 +10,7 @@ import (
 func appHandler(w http.ResponseWriter, r *http.Request) {
 
 	fmt.Println(time.Now(), "Hello from my new fresh server")
-	w.Write([]byte("<h1>Hello from my new fresh server HOHOHO!</h1>"))
+	w.Write([]byte("<h1>Hello from my new fresh server!</h1>"))
 
 }
 
