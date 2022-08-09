2346d8ef88e512feb1a8973832af5d71fc67b792
nafanjka <stas.hrytsyk@gmail.com>
2022-08-09 13:31:12 +0300

    initial

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # final_test_app
 separate app repo
+Application should use 8888 port and return "Hello from my new fresh server!"
---
+++ app/server.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+func appHandler(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Println(time.Now(), "Hello from my new fresh server")
+	w.Write([]byte("<h1>Hello from my new fresh server!</h1>"))
+
+}
+
+func main() {
+	http.HandleFunc("/", appHandler)
+
+	log.Println("Started, serving on port 8888")
+	err := http.ListenAndServe(":8888", nil)
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+}
